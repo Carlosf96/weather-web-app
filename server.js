906a65fe33a19c174d79a22415a67d385a39ba59
Carlosf96 <carlos.f.roque96@gmail.com>
2019-03-23 22:35:09 -0600

    Implemented some esj

--- server.js
+++ server.js
@@ -3,8 +3,9 @@ const app = express();
 const port = 1337;
 const bodyParser = require('body-parser');
 const request = require('request')
-const argv = require('yarg').argv
+const argv = require('yargs').argv
 let apiKey = 'API_KEY';
+app.use(express.static('public'));
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
@@ -13,19 +14,19 @@ app.get('/', (req, res) => res.render('index', {weather:null, error:null}));
 
 app.listen(port, () => console.log(`MyWeather app listening on port ${port}!`));
 
-app.post('/', (req, res) => {
-let city = argv.c || 'portland';
+app.post('/', (req, res, ) => {
+  let city = req.body.city;
   let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`
-  request(url, function (err, resp, body) {
+  request(url, function (err, response, body) {
     if (err) {
-      res.render('error:', error);
+      res.render('index:', {weather: null, error: 'error try again later.'});
     } else {
       let weather = JSON.parse(body);
       //console.log(weather);
       if (weather.main == undefined) res.render('index', { weather: null, error: 'error please try again later' });
       else {
         let weatherText = `It's ${weather.main.temp} degrees in ${weather.name}!`;
-        res.render('index', { weather:weatherText, error: null });
+        res.render('index', { weather: weatherText, error: null });
         //console.log(message);
       }
     }
